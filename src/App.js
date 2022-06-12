3512721b4106beee0b020af64c07843f54738224
IlikedarkWebsite <90788651+IlikedarkWebsite@users.noreply.github.com>
2022-06-12 19:28:26 +0200

    Revert "Strona główna, część"

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./components/Home";
-import Notifications from "./components/Notifications";
-import Profile from "./components/Profile";
-
-//import { ThemeProvider } from "emotion-theming";
-import {
-  createTheme,
-  ThemeProvider as MuiThemeProvider,
-} from "@mui/material/styles";
-import { CssBaseline } from "@mui/material";
-
-const theme = createTheme({});
-
 function App() {
   return (
-    <>
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </MuiThemeProvider>
-    </>
+    <div className="App">
+      Calmstring
+    </div>
   );
 }
 
